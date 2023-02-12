a4c6243c1eeb6a6826bba0ac6e8f50fdb8795595
WilPhil <117986238+WilPhil@users.noreply.github.com>
2023-02-12 16:30:31 +0700

    done

--- Website/main.js
+++ Website/main.js
@@ -1,5 +1,6 @@
 const API_URL = "https://www.themealdb.com/api/json/v1/1/";
 const SEARCH_BY_NAME = "search.php?";
+const MEAL_DETAILS = "lookup.php?";
 
 class Search {
     constructor(form) {
@@ -49,7 +50,49 @@ class Search {
                 <h2>${meal.strMeal}</h2>
                 <img src="${meal.strMealThumb}" alt="${meal.strMeal}"/>
             `;
+            mealElement.addEventListener("click", () => {
+                this.displayMealDetails(meal.idMeal);
+            });
             this.results.appendChild(mealElement);
+        });
+    }
+
+    displayMealDetails(id) {
+        fetch(`${API_URL}${MEAL_DETAILS}` + 
+            new URLSearchParams({
+                i: id,
+            }),
+            {
+                method: "GET",
+            }
+        )
+        .then((response) => {
+            response.json().then((data) => {
+                if (data.meals) {
+                    const meal = data.meals[0];
+                    let ingredients = [];
+                    for (let i = 1; i <= 20; i++) {
+                        if (meal[`strIngredient${i}`]) {
+                            ingredients.push(`${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}`);
+                        }
+                    }
+                    const recipeElement = document.createElement("div");
+                    recipeElement.innerHTML = `
+                        <h2>${meal.strMeal}</h2>
+                        <h3>Ingredients:</h3>
+                        <ul>
+                            ${ingredients.map((ingredient) => `<li>${ingredient}</li>`).join("")}
+                        </ul>
+                        <h3>Instructions:</h3>
+                        <p>${meal.strInstructions}</p>
+                    `;
+                    this.results.innerHTML = "";
+                    this.results.appendChild(recipeElement);
+                }
+            });
+        })
+        .catch((error) => {
+            console.log("error", error);
         });
     }
 }
